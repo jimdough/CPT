0ef6fe22203c6582648acf9feaeeafe0382ec1ef
Jim D <jimdough@Jim-Dough-CPT-MacBook-Pro.local>
2016-04-26 11:58:13 -0400

    new website

---
+++ example.php
@@ -0,0 +1,66 @@
+	    <?php include("inc/head.inc"); ?>
+	    
+	    	    <style>
+		    .row{background-color: green;}
+		</style>
+<p></p>
+<!-- Header and Nav -->  
+<!-- End Header and Nav -->
+<div class="row">
+	<div class="columns small-12 text-center">
+		<img src="http://placehold.it/1200x400&text=[Hero Image]" />
+	</div>
+</div>
+
+<p></p>
+<!-- Three-up Content Blocks -->
+
+	<div style="background-color: orange;" class="row">
+		<div class="large-6 columns">
+      <img src="http://placehold.it/600x300&text=[img]" />
+      <h4>This is a content section.</h4>
+      <p>Bacon ipsum dolor sit amet nulla ham qui sint exercitation eiusmod commodo, chuck duis velit. Aute in reprehenderit, dolore aliqua non est magna in labore pig pork biltong. Eiusmod swine spare ribs reprehenderit culpa. Boudin aliqua adipisicing rump corned beef.</p>
+    </div>
+    
+    <div class="large-6 columns">
+      <img src="http://placehold.it/600x300&text=[img]" />
+      <h4>This is a content section.</h4>
+      <p>Bacon ipsum dolor sit amet nulla ham qui sint exercitation eiusmod commodo, chuck duis velit. Aute in reprehenderit, dolore aliqua non est magna in labore pig pork biltong. Eiusmod swine spare ribs reprehenderit culpa. Boudin aliqua adipisicing rump corned beef.</p>
+    </div>
+    
+
+	
+    </div>
+    
+<!-- Call to Action Panel -->
+<div class="row">
+    <div class="large-12 columns">
+    
+      <div class="panel">
+        <h4>Get in touch!</h4>
+            
+        <div class="row">
+          <div class="large-9 columns">
+            <p>We'd love to hear from you, you attractive person you.</p>
+          </div>
+          <div class="large-3 columns">
+            <a href="#" class="radius button right">Contact Us</a>
+          </div>
+        </div>
+      </div>
+      
+    </div>
+  </div>
+
+  <!-- Footer -->
+  
+  <footer class="row">
+    <div class="large-12 columns">
+      <hr />
+      <div class="row">
+        <div class="large-6 columns">
+          <p>&copy; Copyright no one at all. Go to town.</p>
+        </div>
+    </div> 
+  </footer>
+  <?php include("inc/footer-scripts.inc"); ?>
---
+++ index.php
@@ -0,0 +1,68 @@
+ <head>
+	    <?php include("inc/head.inc"); ?>
+	    <title>Foundation Starter Template</title>
+  </head>
+  
+  <body>
+	  
+<div class="page-wrap">
+<!--First Row-->
+<div class="row">
+	<div class="column" id="header">
+		<img src="http://placehold.it/1200x400&text=[Hero Image]" />
+	</div>
+</div>
+<!--END First Row-->
+
+<!--Second Row-->
+<div class="row" id="hp-features">
+	<div class="columns small-12 medium-6 large-6">
+		<img src="http://placehold.it/500x300&text=[Hero Image]" />
+		<h1>Main Header</h1>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum dui mi, finibus in odio eu, viverra sagittis ex. Etiam a mauris commodo, dapibus mi in, consectetur erat. Suspendisse accumsan libero at finibus mollis. Fusce suscipit enim a massa ullamcorper finibus interdum et nisi. Proin sapien elit, ornare viverra placerat tincidunt, venenatis eu ligula. Morbi mattis ligula in dolor dapibus hendrerit. Donec dictum arcu ut lectus lobortis facilisis. In hac habitasse platea dictumst. Duis condimentum neque faucibus tincidunt elementum. Proin laoreet nisi felis, et pellentesque arcu condimentum sed.</p>
+		<p><a href="#" class="button show-for-medium">Default Button <i class="fa fa-arrow-circle-right"></i></a><a href="#" class="expanded button show-for-small-only">Default Button</a></p>
+	</div>
+
+	<div class="columns small-12 medium-6 large-6 text-center">
+		<img src="http://placehold.it/500x300&text=[Hero Image]" />
+		<h1>Main Header</h1>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum dui mi, finibus in odio eu, viverra sagittis ex. Etiam a mauris commodo, dapibus mi in, consectetur erat. Suspendisse accumsan libero at finibus mollis. Fusce suscipit enim a massa ullamcorper finibus interdum et nisi. Proin sapien elit, ornare viverra placerat tincidunt, venenatis eu ligula. Morbi mattis ligula in dolor dapibus hendrerit. Donec dictum arcu ut lectus lobortis facilisis. In hac habitasse platea dictumst. Duis condimentum neque faucibus tincidunt elementum. Proin laoreet nisi felis, et pellentesque arcu condimentum sed.</p>
+		<p><a href="#" class="button show-for-medium">Default Button <i class="fa fa-arrow-circle-right"></i></a><a href="#" class="expanded button show-for-small-only">Default Button <i class="fa fa-arrow-circle-right"></i></a></p>
+	</div>
+</div>
+<!--END Second Row-->
+
+<!--Footer-->
+<div class="row" id="footer">
+	<div class="columns small-12 medium-5 large-4 logo">
+		<img src="img/logo.png">
+	</div>
+	
+	<div class="columns large-3 show-for-large logo">
+
+	</div>
+
+
+
+	<div class="columns small-12 medium-7 large-5 address">
+		<h3>Career Path Training Corp.</h3>
+			<p><i class="fa fa-home"></i> 11300 4th Street North<br>
+			<span class="shim"></span>Suite 200<br>
+			<span class="shim"></span>St. Petersburg, FL 33716</p>
+			
+		<p><i class="fa fa-phone"></i> <span class="bold">phone:</span> 727-342-6420<br>
+			<i class="fa fa-fax"></i> <span class="bold">fax:</span> 727-342-6421<br>
+			<i class="fa fa-envelope"></i> <span class="bold">email:</span> 
+			<a href="mailto:onlinemarketing@careerpathtraining.com?subject=Question%20from%20Website"> Click Here!</a></p>
+			
+	</div>
+</div>
+
+
+<!-- END Footer-->
+
+
+<?php include("inc/footer-scripts.inc"); ?>
+</div>
+  </body>
+</html>
